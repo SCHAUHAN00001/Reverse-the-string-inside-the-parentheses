a5e12691f99f00e43e1311f9274c78870142d3f2
Shashank Chauhan <shashank.chauhan.ec.2018@miet.ac.in>
2023-06-29 16:01:59 +0530

    Add project files.

---
+++ EXE62/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string result = ReverseStringsInParentheses(input);
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+
+    static string ReverseStringsInParentheses(string input)
+    {
+        StringBuilder output = new StringBuilder();
+        StringBuilder currentString = new StringBuilder();
+
+        foreach (char c in input)
+        {
+            if (c == '(')
+            {
+                output.Append(currentString.ToString());
+                currentString.Clear();
+            }
+            else if (c == ')')
+            {
+                char[] reversedString = currentString.ToString().ToCharArray();
+                Array.Reverse(reversedString);
+                output.Append(new string(reversedString));
+                currentString.Clear();
+            }
+            else
+            {
+                currentString.Append(c);
+            }
+        }
+
+        output.Append(currentString.ToString());
+        return output.ToString();
+    }
+}
+
